91b2bf287d64809f84f46d2347283f1a8159f31d
jeff <jeff@jeffHome.lan>
2018-05-27 09:30:30 +0200

    wip

--- src/components/layout/ProductsList.js
+++ src/components/layout/ProductsList.js
@@ -4,6 +4,60 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { readAllProducts } from '../../actions'
 
+const style = {
+    row: {
+        borderBottom: 'solid 1px #efeded',
+        borderLeft: 'solid 1px #efeded',
+        borderRight: 'solid 1px #efeded',
+    },
+    imgContainer: {
+        width: '200px',
+        height: '150px',
+        backgroundColor: '#efeded',
+        margin: '10px',
+        display: 'inline-block',
+        float: 'left',
+        position: 'relative',
+    },
+    img: {
+        maxWidth: '100%',
+        maxHeight: '100%',
+        margin: 'auto',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+    content: {
+        paddingTop: '15px'
+    },
+    title: {
+        fontSize: '1rem',
+        color: '#369',
+        fontFamily: "'OpenSansSemiBold',sans-serif",
+        fontWeight: 600,
+    },
+    location: {
+        fontWeight: 100,
+        fontSize: '0.8rem',
+    },
+    price: {
+        color: '#f56b2a',
+        fontSize: '1.2rem',
+        fontFamily: "'OpenSansSemiBold',sans-serif",
+        fontWeight: 600,
+    },
+    date: {
+        fontWeight: 100,
+        fontSize: '0.8rem',
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#6c757d'
+    }
+
+}
 class ProductsList extends Component {
 
     constructor(props) {
@@ -17,56 +71,7 @@ class ProductsList extends Component {
 
     renderProducts() {
         const { products } = this.props
-        const style = {
-            row: {
-                borderBottom: 'solid 1px #efeded',
-                borderLeft: 'solid 1px #efeded',
-                borderRight: 'solid 1px #efeded',
-            },
-            imgContainer: {
-                width: '200px',
-                height: '150px',
-                backgroundColor: '#efeded',
-                margin: '10px',
-                display: 'inline-block',
-                float: 'left',
-                position: 'relative',
-            },
-            img: {
-                maxWidth: '100%',
-                maxHeight: '100%',
-                margin: 'auto',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-            },
-            content: {
-                paddingTop: '15px'
-            },
-            title: {
-                fontSize: '1rem',
-                color: '#369',
-                fontFamily: "'OpenSansSemiBold',sans-serif",
-                fontWeight: 600,
-            },
-            location: {
-                fontWeight: 100,
-                fontSize: '0.8rem',
-            },
-            price: {
-                color: '#f56b2a',
-                fontSize: '1.2rem',
-                fontFamily: "'OpenSansSemiBold',sans-serif",
-                fontWeight: 600,
-            },
-            date: {
-                fontWeight: 100,
-                fontSize: '0.8rem',
-            },
-
-        }
+       
         const renderDate = (date) => {
             return (
                 date.toLocaleDateString('fr-FR', options)
@@ -79,21 +84,23 @@ class ProductsList extends Component {
             return (
                 products.map((product) => {
                     return (
-                        <div className="row" style={style.row} key={product.id}>
-                            <div className="col-3" style={style.imgContainer}>
-                                <img src={product.images[0]} alt='' style={style.img} />
+                        <Link to={`product/${product.id}`} key={product.id} style={style.link}>
+                            <div className="row" style={style.row} key={product.id}>
+                                <div className="col-3" style={style.imgContainer}>
+                                    <img src={product.images[0]} alt='' style={style.img} />
+                                </div>
+                                <div className="col-5" style={style.content}>
+                                    <p style={style.title}>{product.title}</p>
+                                    <p style={style.location}>{product.location}</p>
+                                    <p style={style.price}>{isNaN(product.price) ? '' : product.price + ' €'}</p>
+                                </div>
+                                <div className="col-3" style={style.content}>
+                                    <p className="text-right text-muted" style={style.save}><i className="far fa-heart mr-2"></i><i className="far fa-user"></i></p>
+                                    <p className="text-right" style={style.date}>{renderDate(product.date)}</p>
+                                    
+                                </div>
                             </div>
-                            <div className="col-5" style={style.content}>
-                                <p style={style.title}>{product.title}</p>
-                                <p style={style.location}>{product.location}</p>
-                                <p style={style.price}>{isNaN(product.price) ? '' : product.price + ' €'}</p>
-                            </div>
-                            <div className="col-3" style={style.content}>
-                                <p className="text-right text-muted" style={style.save}><i className="far fa-heart mr-2"></i><i className="far fa-user"></i></p>
-                                <p className="text-right" style={style.date}>{renderDate(product.date)}</p>
-                                <Link to={`product/${product.id}`}>détails </Link>
-                            </div>
-                        </div>
+                        </Link>
                     )
                 })
             )
